25b1a214d1a6423addbc6925f9a12f8e46fd7ae4
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-06-27 16:58:22 +0530

    git issue

--- Programming Basics/01_hello_bhaiiyya.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<ncurses.h>  // replacement for " conio --->>> ncurses "
-
-int main()
-{
-    printf("\nHello tejas ! How are you ?");
-    return 0;
-}
---
+++ Programming Basics/01_hello_world.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<ncurses.h>
+
+int main()
+{
+    printf("Gacchu is bacchhu");
+    return 0;
+}
