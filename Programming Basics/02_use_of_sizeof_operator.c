429a066f115006669537c677d21547834cd9aad1
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-06-29 15:44:31 +0530

    necessary comments

--- Programming Basics/02_use_of_sizeof_operator.c
+++ Programming Basics/02_use_of_sizeof_operator.c
@@ -7,5 +7,11 @@ int main()
     printf("The size of float for the machine is %zu bytes\n", sizeof(float));
     printf("The size of character for the machine is %zu bytes", sizeof(char));
 
+/*      The compiler is warning you that you may suffer a loss of precision. 
+    That is, the format specifier that you're using to print a sizeof, %d, 
+    is not capable of printing the full range of size_t. Change %d to %zu and your warning will go away.
+
+    NOTE : The format specifier %d may works for windows but not for linux because of the precision limitation of %d 
+*/
     return 0;
 }               
