84a49a847e693fc2912b57c0cb3dff3d12a430bc
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-08-10 08:55:10 +0530

    commit

--- Test Codes/Basics/02_array_intialization.c
+++ Test Codes/Basics/02_array_intialization.c
@@ -3,16 +3,16 @@
 
 void main()
 {
-    int array[5];
+    int array[5] = {5};
     // An array having size to accomodate 5 integer values in it
 
-    printf("Enter the array elements:\n");
+    printf("Print the array elements:\n");
 
     // Using for loop to store elements into the array
-    for (int j = 0; j < 5; j++)
-    {
-        scanf("%d", &array[j]);
-    }
+    // for (int j = 0; j < 5; j++)
+    // {
+    //     scanf("%d", &array[j]);
+    // }
 
     // accessing the stored array elements
     for (int i = 0; i < 5; i++)
