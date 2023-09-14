9443fe3d85556d64f149de7ac8d78142cb17de7d
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-09-14 19:34:26 +0530

    commit

---
+++ 1. Basics/5. Strings/00_definition_and_initialization.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	// Definition and Initialization of an array
+	// Approach 1
+	char string1 [10] = "Tejas\n";
+	printf("%s",string1);
+
+	// Approach 2
+	// Append a null character at the end of the string characters
+	char string2[10]={'t','e','j','a','s'};
+	printf("%s",string2);
+	
+}
