b868e8554114de91985a21ccc8dc6f6f686e2d06
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-09-16 19:33:16 +0530

    commit

---
+++ 3. Arrays/1. Concept Explained/00_array_declaration_and_definition.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<ncurses.h>
+
+#define m 10
+
+int main()
+{
+    //size in integer format
+    int array1[ 10 ];
+    
+    //size in integer format
+    int array2[ 2 * 5 ];
+
+    //size in character format  >> Unexpected Output/Not a valid syntax
+    int array3[ 'a' ];
+
+    //size in float format  >> Error: Expression must be an integer value
+    //int array3 [ 1.5 ];
+
+    //size in float expression format  >> Error: Expression must evaluate to an integer value
+    //int array3 [ 1 * 2.5 ];
+
+    // USE OF MACRO TO SPECIFY THE SIZE OF THE ARRAY
+    int array6[ m ];
+    
+    return 0;
+}
