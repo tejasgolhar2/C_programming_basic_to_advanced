cbcddf0506012abcb6d438f0d1caa86f554d7ba7
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-09-22 18:45:46 +0530

    commit

---
+++ 2. Strings/1. Concept Explained/04_printing_string_using_puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char nav[6];
+
+	printf("Enter name:\n");
+	scanf("%s",nav);
+
+	// Print using PUTS
+	//  The cursor will be shifted to new line after using puts
+	//  No need to put '\n' operator
+
+	puts(nav);
+	puts(nav);
+	return 0;
+}
