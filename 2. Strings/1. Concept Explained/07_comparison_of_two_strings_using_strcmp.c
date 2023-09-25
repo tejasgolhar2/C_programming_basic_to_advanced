5e4187d5122e2506606c5f727aff63bea5df5ea1
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-09-25 10:30:17 +0530

    commit

---
+++ 2. Strings/1. Concept Explained/07_comparison_of_two_strings_using_strcmp.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+	char name[10]="Tejas";
+	char surname[10]="Tejas";
+
+	//Lets compare the name and surname
+	// 		The strcmp( arg1,arg2 ) function 
+	//		It is used which basically takes the base address of both the strings
+	//   from the base address, it checks character by character whether the characters of both are 
+	//   same or not with the help of corresponding ASCII value of the string
+	//	 It returns -  
+  	//  	0 - when both are same strings
+  	//  	1 - when the ASCII value of the correspoding 1st argument is greater than the 2nd ones ASCII
+  	//  		of the correspoding positioned character
+  	// 		-1 - when the ASCII value of the correspoding 1st argument is lesser than the 2nd ones ASCII
+  	//  		of the correspoding positioned character
+
+	int val = strcmp(name,surname);
+	
+	if (val==0)
+	{
+		printf("Both the strings are same\n");
+	}
+	else if(val==1)
+	{
+		printf("The 1st string's ASCII is found greater than that of 2nd's correspoding character\n");
+	}
+	else
+	{
+		printf("The 1st string's ASCII is found lesser than that of 2nd's correspoding character\n");
+	}
+	return 0;
+}
