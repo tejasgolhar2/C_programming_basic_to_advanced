5fe0950e4305038a02dc79bdb8d7b7d44c03cf45
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-11-03 09:17:40 +0530

    commit

--- 4. Pointers/1. Concept Explained/02_pointer_to_pointer.c
+++ 4. Pointers/1. Concept Explained/02_pointer_to_pointer.c
@@ -11,10 +11,10 @@ int main()
 	int* add = &a;
 
 	// Referencing
-	printf("The address stored by the pointer variable 'add' is %X\n", add );  
+	printf("The address at 'add' is %X\n", add );  
 
 	// Dereferencing
-	printf("The value stored at the address stored by the pointer is %d\n", *add );  
+	printf("The value stored by address at 'add'  is %d\n", *add );  
 
 
 
@@ -24,10 +24,10 @@ int main()
 	int** add2 = &add;
 
 	// Referencing -- Directing to the address where another address is stored
-	printf("The address of the Variable Pointer 'add' is %X and %X\n", &add, add2 );  
+	printf("The address at 'add2' is %X and %X\n", &add, add2 );  
 
 	// Dereferencing -- Getting the value stored at the address stored by Pointer to POinter Variable
-	printf("The value present at the address stored by pointer to pointer variable is %d\n", **add2 );
+	printf("The value present at the address 'add2' is %d\n", **add2 );
 
 	return 0;
 }
--- 4. Pointers/2. Problems Solved/00_modify_value_stored_by_ptr_to_ptr.c
+++ 4. Pointers/2. Problems Solved/00_modify_value_stored_by_ptr_to_ptr.c
@@ -6,13 +6,14 @@ int main()
 
 	int* add = &a;
 	int** add2 = &add;
-
-	printf("The value present at end address of 'add2' is %d\n", **add2 );
-	printf("The address of 'add2' is %X\n", add2 );
+	printf("The value at 'add' is %d\n", *add );
+	printf("The value at 'add2' is %d\n", **add2 );
+	printf("The address in 'add' is %X\n", add );
+	printf("The address in 'add2' is %X\n", add2 );
 
 	// Dereferencing Pointer to POinter
 	**add2 = 101;
 
-	printf("The modified value present at end address of 'add2' is %d\n", **add2 );
+	printf("\nThe modified value at 'add2' is %d\n", **add2 );
 	return 0;
 }
