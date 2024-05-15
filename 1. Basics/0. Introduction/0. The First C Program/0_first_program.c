fd54cd1b8e6048ba37afbaf6067b580ff5a50efa
tejasgolhar2 <tejasgolhar2@gmail.com>
2024-05-15 19:07:21 +0530

    commit

---
+++ 1. Basics/0. Introduction/0. The First C Program/0_first_program.c
@@ -0,0 +1,30 @@
+// Section 1: Documentation Section
+// Agenda: C Programming Basic to Advanced
+// Concept: The Structure of C Program
+// Author: Tejas Golhar
+// Timestamp: 15th May, 2024 18.40 IST
+
+// Section 2: Header/ Link Section
+#include<stdio.h>
+#include<conio.h>
+
+// Section 3: Definition Section
+#define pi 3.14159265359
+
+// Section 4: Global Declaration Section
+// Global variables, function declaration, static variables
+int num;
+void sum();
+
+// Section 5: Main Section
+int main()
+{
+
+	return 0;
+}
+
+// Section 6: Sub-Program Section
+void sum()
+{
+	int val;
+}
